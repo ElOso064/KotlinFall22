11ef177b3cf34803665603c11510aba2f0154d3f
Josue Barajas <111894540+ElOso064@users.noreply.github.com>
2022-09-01 09:35:03 -0500

    Create snipppet022.kt

---
+++ week02/snipppet022.kt
@@ -0,0 +1,11 @@
+ var lucky: Char = '8'
+  println(lucky)
+    
+ var eachDay: Double = 1.0
+  println(eachDay)
+  
+ var age: Int = 88
+  println(age)
+  
+ var kotlinRocks: Boolean = true
+   println(kotlinRocks)
