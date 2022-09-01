0f28d10760cd10c87bc6a9fc32bb2e7190dd7b53
Josue Barajas <111894540+ElOso064@users.noreply.github.com>
2022-09-01 11:39:26 -0500

    Create marketing.kt

---
+++ week02/marketing.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var friendsCount = 554 
+    if (friendsCount > 500) {
+        println("Adop a Dog today!")
+    } 
+    if (friendsCount < 500) {
+        println("Buy Cat Food at 20% off.")
+    }
+    
+}
