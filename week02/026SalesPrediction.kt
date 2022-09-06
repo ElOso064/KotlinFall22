4141fcc17462e2804ea6d9caa670084fc43da531
Josue Barajas <111894540+ElOso064@users.noreply.github.com>
2022-09-06 10:16:01 -0500

    Create 026SalesPrediction.kt

---
+++ week02/026SalesPrediction.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+	val totalSales = 4600000.00
+	val eastCoastPercentage = 0.62
+    val eastCoastTotalProfit = totalSales * eastCoastPercentage
+    
+    println("The companies total sales this year was $" +totalSales+ ". The East Coast Division contributed a total of $" 
+           +eastCoastTotalProfit+ ".") 
+}
