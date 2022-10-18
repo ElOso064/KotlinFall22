7a94e2cded875beb07d2a9a64001724b0dc27a99
Josue Barajas <111894540+ElOso064@users.noreply.github.com>
2022-10-18 08:44:51 -0500

    Create error.kt

---
+++ week01/error.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var width = 5
+    var height = 5
+    // the error was that it was adding the width + height, instead of multiplying
+    var area = width*height
+    print(area)
+}
